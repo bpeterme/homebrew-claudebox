d35b8182c0f0203fdda632025f05a9eda0e3666e
bpeterme <b.petermeier@gmail.com>
2026-05-18 19:04:08 +0900

    Update claudebox.rb
    head block pointing at the dev branch

--- Formula/claudebox.rb
+++ Formula/claudebox.rb
@@ -5,17 +5,22 @@ class Claudebox < Formula
   sha256 "PLACEHOLDER"
   license "MIT"
 
+  head "https://github.com/bpeterme/claudebox.git", branch: "dev"
+
   depends_on :macos
-  depends_on "bash"
   depends_on "jq"
 
   def install
+    version_str = build.head? ? `git describe --tags --always`.chomp : version.to_s
+    inreplace "cbox.sh", '_CBOX_VERSION="dev"', "_CBOX_VERSION=\"#{version_str}\""
     bin.install "cbox.sh" => "cbox"
     (share/"claudebox").install "dockerfile"
   end
 
   def caveats
     <<~EOS
+      claudebox requires Apple Container (macOS) or Docker to run containers.
+
       Build the container image before first use:
         cbox rebuild
     EOS
